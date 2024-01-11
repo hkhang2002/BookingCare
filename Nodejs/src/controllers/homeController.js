37a0c627461e3a2e37e66ee81b0eab8e2b63c583
hkhang2002 <hungkhang2002@gmail.com>
2024-01-11 16:53:31 +0700

    crud

--- Nodejs/src/controllers/homeController.js
+++ Nodejs/src/controllers/homeController.js
@@ -1,19 +1,29 @@
 import db from '../models/index';
-let getHomePage = async (req,res) =>{
-    try
-    {
+import CRUDService from '../services/CRUDService';
+let getHomePage = async (req, res) => {
+    try {
         let data = await db.User.findAll();
         return res.render('homepage.ejs', {
             data: JSON.stringify(data)
         })
-    } catch(e)
-
-    {
+    } catch (e) {
         console.log(e)
     }
-    
+
+}
+
+let getCRUD = (req, res) => {
+    return res.render('crud.ejs');
+}
+
+let postCRUD = async (req, res) => {
+    let message = await CRUDService.createNewUser(req.body);
+    console.log(message);
+    return res.send('asdsadas')
 }
 
 module.exports = {
     getHomePage: getHomePage,
+    getCRUD: getCRUD,
+    postCRUD: postCRUD,
 }
